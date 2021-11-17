7427e4fc1df6b8ea79f2421539db420bd617f5af
Дарина Мадуар <maduardar@yandex.ru>
2021-11-17 05:58:38 +0300

    Add files via upload

--- OnlineShop/dbaccess.py
+++ OnlineShop/dbaccess.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 def gen_custID():
     conn = sqlite3.connect("OnlineShop/onlineshop.db")
     cur = conn.cursor()
@@ -7,8 +8,9 @@ def gen_custID():
     conn.commit()
     custnum = str([i for i in cur.execute("SELECT custnum FROM metadata")][0][0])
     conn.close()
-    id = "CID"+"0"*(7-len(custnum))+custnum
+    id = "CID" + "0" * (7 - len(custnum)) + custnum
     return id
+
 
 def gen_sellID():
     conn = sqlite3.connect("OnlineShop/onlineshop.db")
@@ -17,8 +19,9 @@ def gen_sellID():
     conn.commit()
     sellnum = str([i for i in cur.execute("SELECT sellnum FROM metadata")][0][0])
     conn.close()
-    id = "SID"+"0"*(7-len(sellnum))+sellnum
+    id = "SID" + "0" * (7 - len(sellnum)) + sellnum
     return id
+
 
 def gen_prodID():
     conn = sqlite3.connect("OnlineShop/onlineshop.db")
@@ -27,8 +30,9 @@ def gen_prodID():
     conn.commit()
     prodnum = str([i for i in cur.execute("SELECT prodnum FROM metadata")][0][0])
     conn.close()
-    id = "PID"+"0"*(7-len(prodnum))+prodnum
+    id = "PID" + "0" * (7 - len(prodnum)) + prodnum
     return id
+
 
 def gen_orderID():
     conn = sqlite3.connect("OnlineShop/onlineshop.db")
@@ -37,36 +41,38 @@ def gen_orderID():
     conn.commit()
     ordernum = str([i for i in cur.execute("SELECT ordernum FROM metadata")][0][0])
     conn.close()
-    id = "OID"+"0"*(7-len(ordernum))+ ordernum
+    id = "OID" + "0" * (7 - len(ordernum)) + ordernum
     return id
+
 
 def add_user(data):
     conn = sqlite3.connect("OnlineShop/onlineshop.db")
     cur = conn.cursor()
     email = data["email"]
-    if data['type']=="Customer":
+    if data['type'] == "Customer":
         a = cur.execute("SELECT * FROM customer WHERE email=?", (email,))
-    elif data['type']=="Seller":
+    elif data['type'] == "Seller":
         a = cur.execute("SELECT * FROM seller WHERE email=?", (email,))
-    if len(list(a))!=0:
+    if len(list(a)) != 0:
         return False
-    tup = ( data["name"],
-            data["email"],
-            data["phone"],
-            data["area"],
-            data["locality"],
-            data["city"],
-            data["state"],
-            data["country"],
-            data["zip"],
-            data["password"])
-    if data['type']=="Customer":
-        cur.execute("INSERT INTO customer VALUES (?,?,?,?,?,?,?,?,?,?,?)",(gen_custID(), *tup))
-    elif data['type']=="Seller":
+    tup = (data["name"],
+           data["email"],
+           data["phone"],
+           data["area"],
+           data["locality"],
+           data["city"],
+           data["state"],
+           data["country"],
+           data["zip"],
+           data["password"])
+    if data['type'] == "Customer":
+        cur.execute("INSERT INTO customer VALUES (?,?,?,?,?,?,?,?,?,?,?)", (gen_custID(), *tup))
+    elif data['type'] == "Seller":
         cur.execute("INSERT INTO seller VALUES (?,?,?,?,?,?,?,?,?,?,?)", (gen_sellID(), *tup))
     conn.commit()
     conn.close()
     return True
+
 
 def auth_user(data):
     conn = sqlite3.connect("OnlineShop/onlineshop.db")
@@ -74,87 +80,101 @@ def auth_user(data):
     type = data["type"]
     email = data["email"]
     password = data["password"]
-    if type=="Customer":
+    if type == "Customer":
         a = cur.execute("SELECT custID, name FROM customer WHERE email=? AND password=?", (email, password))
-    elif type=="Seller":
+    elif type == "Seller":
         a = cur.execute("SELECT sellID, name FROM seller WHERE email=? AND password=?", (email, password))
     a = list(a)
     conn.close()
-    if len(a)==0:
+    if len(a) == 0:
         return False
     return a[0]
+
 
 def fetch_details(userid, type):
     conn = sqlite3.connect("OnlineShop/onlineshop.db")
     cur = conn.cursor()
-    if type=="Customer":
+    if type == "Customer":
         a = cur.execute("SELECT * FROM customer WHERE custID=?", (userid,))
         a = list(a)
         b = []
-    elif type=="Seller":
+    elif type == "Seller":
         a = cur.execute("SELECT * FROM seller WHERE sellID=?", (userid,))
         a = list(a)
         b = cur.execute("SELECT DISTINCT(category) from product WHERE sellID=?", (userid,))
-        b = [i[0] for i in b ]
+        b = [i[0] for i in b]
     conn.close()
     return a, b
+
 
 def search_users(search, srch_type):
     conn = sqlite3.connect('OnlineShop/onlineshop.db')
     cur = conn.cursor()
-    search = "%"+search+"%"
-    if srch_type=="Customer":
-        res = cur.execute("SELECT custID, name, email, phone, area, locality, city, state, country, zipcode FROM customer WHERE LOWER(name) like ?", (search,))
-    elif srch_type=="Seller":
-        res = cur.execute("SELECT sellID, name, email, phone, area, locality, city, state, country, zipcode FROM seller WHERE LOWER(name) like ?", (search,))
-    res = [i for i in res ]
+    search = "%" + search + "%"
+    if srch_type == "Customer":
+        res = cur.execute(
+            "SELECT custID, name, email, phone, area, locality, city, state, country, zipcode FROM customer WHERE LOWER(name) like ?",
+            (search,))
+    elif srch_type == "Seller":
+        res = cur.execute(
+            "SELECT sellID, name, email, phone, area, locality, city, state, country, zipcode FROM seller WHERE LOWER(name) like ?",
+            (search,))
+    res = [i for i in res]
     conn.close()
     return res
+
 
 def update_details(data, userid, type):
     conn = sqlite3.connect("OnlineShop/onlineshop.db")
     cur = conn.cursor()
-    if type=="Customer":
-        cur.execute("UPDATE customer SET phone=?, area=?, locality=?, city=?, state=?, country=?, zipcode=? where custID=?", (data["phone"],
-                    data["area"],
-                    data["locality"],
-                    data["city"],
-                    data["state"],
-                    data["country"],
-                    data["zip"],
-                    userid))
-    elif type=="Seller":
-        cur.execute("UPDATE seller SET phone=?, area=?, locality=?, city=?, state=?, country=?, zipcode=? where sellID=?", (data["phone"],
-                    data["area"],
-                    data["locality"],
-                    data["city"],
-                    data["state"],
-                    data["country"],
-                    data["zip"],
-                    userid))
+    if type == "Customer":
+        cur.execute(
+            "UPDATE customer SET phone=?, area=?, locality=?, city=?, state=?, country=?, zipcode=? where custID=?",
+            (data["phone"],
+             data["area"],
+             data["locality"],
+             data["city"],
+             data["state"],
+             data["country"],
+             data["zip"],
+             userid))
+    elif type == "Seller":
+        cur.execute(
+            "UPDATE seller SET phone=?, area=?, locality=?, city=?, state=?, country=?, zipcode=? where sellID=?",
+            (data["phone"],
+             data["area"],
+             data["locality"],
+             data["city"],
+             data["state"],
+             data["country"],
+             data["zip"],
+             userid))
     conn.commit()
     conn.close()
+
 
 def check_psswd(psswd, userid, type):
     conn = sqlite3.connect("OnlineShop/onlineshop.db")
     cur = conn.cursor()
-    if type=="Customer":
+    if type == "Customer":
         a = cur.execute("SELECT password FROM customer WHERE custID=?", (userid,))
-    elif type=="Seller":
+    elif type == "Seller":
         a = cur.execute("SELECT password FROM seller WHERE sellID=?", (userid,))
     real_psswd = list(a)[0][0]
     conn.close()
-    return psswd==real_psswd
+    return psswd == real_psswd
+
 
 def set_psswd(psswd, userid, type):
     conn = sqlite3.connect("OnlineShop/onlineshop.db")
     cur = conn.cursor()
-    if type=="Customer":
+    if type == "Customer":
         a = cur.execute("UPDATE customer SET password=? WHERE custID=?", (psswd, userid))
-    elif type=="Seller":
+    elif type == "Seller":
         a = cur.execute("UPDATE seller SET password=? WHERE sellID=?", (psswd, userid))
     conn.commit()
     conn.close()
+
 
 def add_prod(sellID, data):
     conn = sqlite3.connect("OnlineShop/onlineshop.db")
@@ -172,6 +192,7 @@ def add_prod(sellID, data):
     conn.commit()
     conn.close()
 
+
 def get_categories(sellID):
     conn = sqlite3.connect("OnlineShop/onlineshop.db")
     cur = conn.cursor()
@@ -180,16 +201,17 @@ def get_categories(sellID):
     conn.close()
     return categories
 
+
 def search_myproduct(sellID, srchBy, category, keyword):
     conn = sqlite3.connect("OnlineShop/onlineshop.db")
     cur = conn.cursor()
-    keyword = ['%'+i+'%' for i in keyword.split()]
-    if len(keyword)==0: keyword.append('%%')
-    if srchBy=="by category":
+    keyword = ['%' + i + '%' for i in keyword.split()]
+    if len(keyword) == 0: keyword.append('%%')
+    if srchBy == "by category":
         a = cur.execute("""SELECT prodID, name, quantity, category, cost_price
-                        FROM product WHERE category=? AND sellID=? """,(category, sellID))
+                        FROM product WHERE category=? AND sellID=? """, (category, sellID))
         res = [i for i in a]
-    elif srchBy=="by keyword":
+    elif srchBy == "by keyword":
         res = []
         for word in keyword:
             a = cur.execute("""SELECT prodID, name, quantity, category, cost_price
@@ -198,7 +220,7 @@ def search_myproduct(sellID, srchBy, category, keyword):
                             (word, word, word, sellID))
             res += list(a)
         res = list(set(res))
-    elif srchBy=="both":
+    elif srchBy == "both":
         res = []
         for word in keyword:
             a = cur.execute("""SELECT prodID, name, quantity, category, cost_price
@@ -210,6 +232,7 @@ def search_myproduct(sellID, srchBy, category, keyword):
     conn.close()
     return res
 
+
 def get_product_info(id):
     conn = sqlite3.connect('OnlineShop/onlineshop.db')
     cur = conn.cursor()
@@ -218,9 +241,10 @@ def get_product_info(id):
                     WHERE p.sellID=s.sellID AND p.prodID=? """, (id,))
     res = [i for i in a]
     conn.close()
-    if len(res)==0:
+    if len(res) == 0:
         return False, res
     return True, res[0]
+
 
 def update_product(data, id):
     conn = sqlite3.connect('OnlineShop/onlineshop.db')
@@ -228,7 +252,7 @@ def update_product(data, id):
     cur.execute("""UPDATE product
     SET name=?, quantity=?, category=?, cost_price=?,
     sell_price=(SELECT profit_rate from metadata)*?, description=?
-    where prodID=?""",( data['name'],
+    where prodID=?""", (data['name'],
                         data['qty'],
                         data['category'],
                         data['price'],
@@ -238,16 +262,17 @@ def update_product(data, id):
     conn.commit()
     conn.close()
 
+
 def search_products(srchBy, category, keyword):
     conn = sqlite3.connect("OnlineShop/onlineshop.db")
     cur = conn.cursor()
-    keyword = ['%'+i+'%' for i in keyword.split()]
-    if len(keyword)==0: keyword.append('%%')
-    if srchBy=="by category":
+    keyword = ['%' + i + '%' for i in keyword.split()]
+    if len(keyword) == 0: keyword.append('%%')
+    if srchBy == "by category":
         a = cur.execute("""SELECT prodID, name, category, sell_price
-                        FROM product WHERE category=? AND quantity!=0 """,(category,))
+                        FROM product WHERE category=? AND quantity!=0 """, (category,))
         res = [i for i in a]
-    elif srchBy=="by keyword":
+    elif srchBy == "by keyword":
         res = []
         for word in keyword:
             a = cur.execute("""SELECT prodID, name, category, sell_price
@@ -256,7 +281,7 @@ def search_products(srchBy, category, keyword):
                             (word, word, word))
             res += list(a)
         res = list(set(res))
-    elif srchBy=="both":
+    elif srchBy == "both":
         res = []
         for word in keyword:
             a = cur.execute("""SELECT prodID, name, category, sell_price
@@ -268,6 +293,7 @@ def search_products(srchBy, category, keyword):
     conn.close()
     return res
 
+
 def get_seller_products(sellID):
     conn = sqlite3.connect('OnlineShop/onlineshop.db')
     cur = conn.cursor()
@@ -275,6 +301,7 @@ def get_seller_products(sellID):
     res = [i for i in a]
     conn.close()
     return res
+
 
 def place_order(prodID, custID, qty):
     conn = sqlite3.connect('OnlineShop/onlineshop.db')
@@ -285,6 +312,7 @@ def place_order(prodID, custID, qty):
                     FROM product WHERE prodID=? """, (orderID, custID, prodID, qty, qty, qty, prodID))
     conn.commit()
     conn.close()
+
 
 def cust_orders(custID):
     conn = sqlite3.connect('OnlineShop/onlineshop.db')
@@ -297,6 +325,7 @@ def cust_orders(custID):
     conn.close()
     return res
 
+
 def sell_orders(sellID):
     conn = sqlite3.connect('OnlineShop/onlineshop.db')
     cur = conn.cursor()
@@ -308,6 +337,7 @@ def sell_orders(sellID):
     conn.close()
     return res
 
+
 def get_order_details(orderID):
     conn = sqlite3.connect('OnlineShop/onlineshop.db')
     cur = conn.cursor()
@@ -317,16 +347,18 @@ def get_order_details(orderID):
     conn.close()
     return res
 
+
 def change_order_status(orderID, new_status):
     conn = sqlite3.connect('OnlineShop/onlineshop.db')
     cur = conn.cursor()
     cur.execute("UPDATE orders SET status=? WHERE orderID=? ", (new_status, orderID))
-    if new_status=='DISPACHED':
+    if new_status == 'DISPACHED':
         cur.execute("""UPDATE product SET
                      quantity=quantity-(SELECT quantity FROM orders WHERE orderID=? )
                      WHERE prodID=(SELECT prodID FROM orders WHERE orderID=? )""", (orderID, orderID))
     conn.commit()
     conn.close()
+
 
 def cust_purchases(custID):
     conn = sqlite3.connect('OnlineShop/onlineshop.db')
@@ -339,6 +371,7 @@ def cust_purchases(custID):
     conn.close()
     return res
 
+
 def sell_sales(sellID):
     conn = sqlite3.connect('OnlineShop/onlineshop.db')
     cur = conn.cursor()
@@ -350,12 +383,14 @@ def sell_sales(sellID):
     conn.close()
     return res
 
+
 def add_product_to_cart(prodID, custID):
     conn = sqlite3.connect('OnlineShop/onlineshop.db')
     cur = conn.cursor()
     cur.execute("""INSERT INTO cart VALUES (?,?,1) """, (custID, prodID))
     conn.commit()
     conn.close()
+
 
 def get_cart(custID):
     conn = sqlite3.connect('OnlineShop/onlineshop.db')
@@ -368,6 +403,7 @@ def get_cart(custID):
     conn.close()
     return res
 
+
 def update_cart(custID, qty):
     conn = sqlite3.connect('OnlineShop/onlineshop.db')
     cur = conn.cursor()
@@ -376,6 +412,7 @@ def update_cart(custID, qty):
         cur.execute("INSERT INTO cart VALUES (?,?,?)", (custID, prodID, qty[prodID]))
     conn.commit()
     conn.close()
+
 
 def cart_purchase(custID):
     conn = sqlite3.connect('OnlineShop/onlineshop.db')
@@ -392,15 +429,16 @@ def cart_purchase(custID):
         conn.commit()
     conn.close()
 
+
 def empty_cart(custID):
     conn = sqlite3.connect('OnlineShop/onlineshop.db')
     cur = conn.cursor()
     cur.execute("DELETE FROM cart WHERE custID=?", (custID,))
     conn.commit()
 
+
 def remove_from_cart(custID, prodID):
     conn = sqlite3.connect('OnlineShop/onlineshop.db')
     cur = conn.cursor()
     cur.execute("DELETE FROM cart WHERE custID=? AND prodID=?", (custID, prodID))
     conn.commit()
-    
